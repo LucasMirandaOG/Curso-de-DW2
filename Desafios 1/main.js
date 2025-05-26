a600214f01f2b641b3d1dc993ecf467f99eeee89
lucas <lap23miranda@hotmail.com>
2025-05-26 18:39:16 -0300

    nuevo

---
+++ Desafios 1/main.js
@@ -0,0 +1,10 @@
+let nombreProducto = "Leche";
+
+let precioUnitario = 1.329;
+
+let cantidadDeseada = prompt("¿Qué cantidad desea comprar?");
+
+let precioFinal = cantidadDeseada * precioUnitario;
+
+alert("Este es el costo de su compra: $" + precioFinal)
+
---
+++ assets/index.js
@@ -0,0 +1,1 @@
+alert("Que desea comprar?")
