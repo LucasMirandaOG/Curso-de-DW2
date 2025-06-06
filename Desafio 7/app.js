2dfe20384d0f14a5bd3d207ea7c1de47db33165e
lucas <lap23miranda@hotmail.com>
2025-06-06 20:42:55 -0300

    Desafio 7 completo

---
+++ Desafio 7/app.js
@@ -0,0 +1,9 @@
+let precioProducto = document.getElementById("precioProducto");
+
+precioProducto.innerText = "Precio: $450";
+
+const titulo = document.querySelector("h1");
+
+titulo.style.color = "green";
+
+titulo.style.fontFamily = "Roboto Condensed";
